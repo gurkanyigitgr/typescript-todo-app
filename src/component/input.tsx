e52a5327cc7d6a0b3c2b859af2a59b8190c3721d
Gürkan <gurkanyigitgr@gmail.com>
2023-07-26 12:19:05 +0300

    add onChange method for input component

--- src/component/input.tsx
+++ src/component/input.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 const style = {
   form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `p-4 ml-2 bg-purple-500 text-slate-100 rounded-lg font-semibold`,
+  input: `border p-2 w-full text-xl mr-5 rounded-lg`,
+  button: `border-none w-[150px] h-[50px] bg-gradient-to-r from-purple-300 to-purple-500 rounded-lg text-white font-semibold`,
 };
 
-const Input = () => {
+type todoProps = {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  addMessage: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const Input: React.FC<todoProps> = ({ todo, setTodo, addMessage }) => {
   return (
     <>
-      <form className={style.form}>
-        <input className={style.input} type="text" placeholder="Add Todo" />
-        <button className={style.button}>Add</button>
+      <form className={style.form} onSubmit={addMessage}>
+        <input
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          className={style.input}
+          type="text"
+          placeholder="Add Todo"
+        />
+        <button type="submit" className={style.button}>
+          Add
+        </button>
       </form>
     </>
   );
