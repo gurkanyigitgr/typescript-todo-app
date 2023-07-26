5a912624f6926c2d708956b3fd55cefc76d0c8a6
Gürkan <gurkanyigitgr@gmail.com>
2023-07-26 12:18:37 +0300

    add addMessage and deleteMessage func

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./component/input";
 import Todo from "./component/todo";
+import { Todos } from "./types/Type";
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#A1CCD1] to-[#7C9D96]`,
   container: `bg-slate-100 max-w-[500px] w-full mx-auto rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
-  form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100 rounded-lg `,
-  buttonSec: `border-none w-[150px] h-[50px] bg-gradient-to-r from-purple-300 to-purple-500 rounded-lg text-white font-semibold`,
+  button: `border-none w-[150px] h-[50px] bg-gradient-to-r from-purple-300 to-purple-500 rounded-lg text-white font-semibold`,
   count: `text-center p-2`,
   centeredDiv: `flex flex-col space-y-5 items-center justify-center mt-5 mb-5 md:flex-row md:space-x-10 md:space-y-0`,
 };
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todoArr, setTodoArr] = useState<Todos[]>([]);
+
+  const addMessage = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo)
+      setTodoArr([...todoArr, { message: todo, id: todoArr.length + 1 }]);
+    setTodo("");
+  };
+
+  const deleteMessage = (id: number) => {
+    setTodoArr(todoArr.filter((todo) => todo.id != id));
+  };
+
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h1 className={style.heading}>Todo App</h1>
-        <Input />
+        <Input addMessage={addMessage} todo={todo} setTodo={setTodo} />
         <ul>
-          <Todo />
+          <Todo deleteMessage={deleteMessage} todoArr={todoArr} />
         </ul>
-        <p className={style.count}>You have 4 todos</p>
-        <div className={style.centeredDiv}>
-          <button className={style.buttonSec}>Active</button>
-          <button className={style.buttonSec}>Completed</button>
-          <button className={style.buttonSec}>All Todos</button>
-        </div>
+        <p className={style.count}>You have {todoArr.length} todo</p>
       </div>
     </div>
   );
-}
+};
 
 export default App;
