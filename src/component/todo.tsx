10d3892079a2cb83eb0e87884ed4baa85277f39b
Gürkan <gurkanyigitgr@gmail.com>
2023-07-26 12:19:19 +0300

    add todo component

--- src/component/todo.tsx
+++ src/component/todo.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Todos } from "../types/Type";
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+};
 
-const todo = () => {
-  return <div>todo</div>;
+type Props = {
+  todoArr: Todos[];
+  deleteMessage: (id: number) => void;
+};
+
+const todo: React.FC<Props> = ({ todoArr, deleteMessage }) => {
+  return (
+    <>
+      {todoArr.map((todo, i) => (
+        <div key={i}>
+          <li className={style.li}>
+            {todo.message}
+            <span
+              className="cursor-pointer text-2xl font-bold"
+              onClick={() => deleteMessage(todo.id)}
+            >
+              {" "}
+              X{" "}
+            </span>
+          </li>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default todo;
